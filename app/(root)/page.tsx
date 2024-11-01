ebe0febfb11112338976814034b23480e8333e8b
John Name <oluwatobilobasalawu1234@gmail.com>
2024-11-01 19:44:08 +0000

    adding header components - TotalBalancebox

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,25 @@
 import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React  from "react";
 
 const Home = () => {
+  const loggedIn = {firstName: "Sarah"};
+
   return (
     <section className="home">
       <div className="home-content">
         <header className="home-header">
           <HeaderBox 
-          
+            type="greeting"
+            title="Welcome"
+            user={loggedIn ? loggedIn.firstName : "Guest"}
+            subtext = "Access and manage your account including transactions effectively."
+          />
+
+          <TotalBalanceBox 
+            accounts = {[]}
+            totalBanks = {1}
+            totalCurrentBalance = {7800.58}
           />
         </header>
       </div>
--- components/HeaderBox.tsx
+++ components/HeaderBox.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 
-const HeaderBox = () => {
+const HeaderBox = ( {type = "title", title, user, subtext}: HeaderBoxProps) => {
   return (
-    <div>HeaderBox</div>
+    <div className="header-box">
+      <h1 className="header-box-title">{title}
+        {type === 'greeting' && (
+          <span className="text-bankGradient">&nbsp;{user}</span>
+        )}
+      </h1>
+      <p className="header-box-subtext">{subtext}</p>
+    </div>
   )
 }
 
