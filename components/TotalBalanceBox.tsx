1fde2fb72da9b76fdf24b3c26a9f9855aece5dd1
John Name <oluwatobilobasalawu1234@gmail.com>
2024-11-01 20:09:05 +0000

    Implementing the countdown using count-up

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -1,8 +1,31 @@
 import React from "react";
+import { formatAmount } from "@/lib/utils";
+import CountUp from "react-countup";
+import AnimatedCounter from "./AnimatedCounter";
 
-const TotalBalanceBox = () => {
+const TotalBalanceBox = ({accounts = [], totalBanks = 0, totalCurrentBalance = 0}: TotalBalanceBoxProps) => {
+
   return (
-    <div>TotalBalanceBox</div>
+    <section className="total-balance">
+      <div className="total-balance-chart">
+        {/* Doughnut Chart */}
+      </div>
+
+      <div className="flex flex-col gap-6">
+        <h2 className="header-2">
+          Bank Accounts: {totalBanks}
+        </h2>
+        <div className="flex flex-col gap-2">
+          <p className = "total-balance-label">
+            Total Current Balance
+          </p>
+
+          <p className = "total-balance-amount flex-center gap-2">
+            <AnimatedCounter amount = {totalCurrentBalance} />
+          </p>
+        </div>
+      </div>
+    </section>
   )
 }
 
