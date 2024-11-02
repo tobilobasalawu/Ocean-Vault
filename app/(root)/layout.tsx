0b1c7f101144107da9646455e291d4479cf800e5
John Name <oluwatobilobasalawu1234@gmail.com>
2024-11-02 22:41:36 +0000

    completed structuring mobile nav

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import Sidebar from "@/components/Sidebar";
 import Image from "next/image";
+import MobileNav from "@/components/MobileNavbar";
 
 export default function RootLayout({
   children,
@@ -15,12 +16,12 @@ export default function RootLayout({
       <div className = "root-layout">
         <Image src="/icons/logo.svg" alt="Menu Icon" width={34} height={34}/>
         <div>
-          
+          <MobileNav user = {loggedIn}/>
         </div>
       </div>
-    </div>
-
       {children}
+    </div>
+    
     </main>
   );
 }
---
+++ components/MobileNavbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MobileNav = ({user}: MobileNavProps) => {
+    return (
+        <div>
+          Mobile Navbar
+        </div>
+    )
+}
+
+export default MobileNav;
