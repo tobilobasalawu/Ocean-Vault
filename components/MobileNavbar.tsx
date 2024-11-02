8bc38656191805c5ec7728473447327614858b74
John Name <oluwatobilobasalawu1234@gmail.com>
2024-11-02 23:24:59 +0000

    completed setting up the links on the sidebar

--- components/MobileNavbar.tsx
+++ components/MobileNavbar.tsx
@@ -40,15 +40,16 @@ const MobileNav = ({user}: MobileNavProps) => {
                     const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`);
 
                     return (
-                      <Link href={item.route} key = {item.label} 
-                        className = {cn ('sidebar-link', {'bg-bank-gradient' : isActive})}>
-                        <div className="relative size-6">
-                          <Image src={item.imgURL} alt={item.label} fill className={cn ({'brightness-[3] invert-0': isActive})}/>
-                        </div>
-                        <p className={cn ('sidebar-label', {'!text-white': isActive})}>
-                          {item.label}
-                        </p>
-                      </Link>
+                      <SheetClose asChild key={item.route}>
+                        <Link href={item.route} key = {item.label} 
+                          className = {cn ('mobilenav-sheet_close w-full', {'bg-bank-gradient' : isActive})}>
+                            <Image src={item.imgURL} alt={item.label} width={20} height={20} className={cn ({'brightness-[3] invert-0': isActive})}/>
+                          <p className={cn ('text-16 font-semibold text-black-2', {'!text-white': isActive})}>
+                            {item.label}
+                          </p>
+                        </Link>
+                      </SheetClose>
+
                     )
                     })}
                 </nav>
