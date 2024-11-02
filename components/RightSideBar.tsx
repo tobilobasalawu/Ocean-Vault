9a66e9c3415430680f17a9ef372c3b051c30db7d
John Name <oluwatobilobasalawu1234@gmail.com>
2024-11-02 23:59:21 +0000

    setting banks scetion on the right

--- components/RightSideBar.tsx
+++ components/RightSideBar.tsx
@@ -4,8 +4,14 @@ const RightSideBar = ({user, transactions, banks} : RightSideBarProps) => {
   return (
     <aside className="right-sidebar">
       <section className="flex flex-col pb-8">
+        {/* class css in global.css, the className is the name of the class in the global.css stylesheet */}
         <div className="profile-banner">
-          <div></div>
+        </div>
+      </section>
+
+      <section className="banks-section">
+        <div className="flex w-full justify-between">
+          <h2 className="header-2">Banks</h2>	
         </div>
       </section>
     </aside>
