eadb8b2213df17534577345260c6e83053f505c3
John Name <oluwatobilobasalawu1234@gmail.com>
2024-11-02 23:06:02 +0000

    structuring the link on the sidebar left side

--- components/MobileNavbar.tsx
+++ components/MobileNavbar.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import {
   Sheet,
+  SheetClose,
   SheetContent,
   SheetDescription,
   SheetHeader,
@@ -23,30 +24,36 @@ const MobileNav = ({user}: MobileNavProps) => {
             <SheetTrigger>
               <Image src="/icons/hamburger.svg" alt="Menu" width={30} height={30}/>
             </SheetTrigger>
-            <SheetContent side = "left">
-            <Link href="/" className="mb-12 cursor-pointer flex items-center gap-2">
-              <Image src="/icons/logo.svg" alt="Ocean-Vault Logo" width={34} height={34} className="size-[24px] max-xl:size-14"/>
-              <h1 className="sidebar-logo">
+            <SheetContent side = "left" className = "border-none bg-white"> 
+            <Link href="/" className="cursor-pointer flex items-center gap-1 px-4">
+              <Image src="/icons/logo.svg" alt="Ocean-Vault Logo" width={34} height={34}/>
+              <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">
                 Ocean Vault
               </h1>
             </Link>
 
-            {sidebarLinks.map((item) => {
+            <div className = "mobilenav-sheet">
+              <SheetClose asChild>
+                <nav className= "flex h-full flex-col gap-6 pt-16 text-white">
+                  {sidebarLinks.map((item) => {
 
-              const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`);
+                    const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`);
 
-              return (
-                <Link href={item.route} key = {item.label} 
-                  className = {cn ('sidebar-link', {'bg-bank-gradient' : isActive})}>
-                  <div className="relative size-6">
-                    <Image src={item.imgURL} alt={item.label} fill className={cn ({'brightness-[3] invert-0': isActive})}/>
-                  </div>
-                  <p className={cn ('sidebar-label', {'!text-white': isActive})}>
-                    {item.label}
-                  </p>
-                </Link>
-          )
-        })}
+                    return (
+                      <Link href={item.route} key = {item.label} 
+                        className = {cn ('sidebar-link', {'bg-bank-gradient' : isActive})}>
+                        <div className="relative size-6">
+                          <Image src={item.imgURL} alt={item.label} fill className={cn ({'brightness-[3] invert-0': isActive})}/>
+                        </div>
+                        <p className={cn ('sidebar-label', {'!text-white': isActive})}>
+                          {item.label}
+                        </p>
+                      </Link>
+                    )
+                    })}
+                </nav>
+              </SheetClose>
+            </div>
             </SheetContent>
           </Sheet>
         </section>
