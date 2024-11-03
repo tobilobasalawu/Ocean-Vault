97974b4ca809c858e265e2f36a8edb9f91f50b9d
John Name <oluwatobilobasalawu1234@gmail.com>
2024-11-03 00:12:26 +0000

    setting up layout for banks section within the right sidebar

--- components/RightSideBar.tsx
+++ components/RightSideBar.tsx
@@ -16,9 +16,23 @@ const RightSideBar = ({user, transactions, banks} : RightSideBarProps) => {
           <h2 className="header-2">My Banks</h2>
           <Link href="/" className="flex gap-2">
             <Image src="/icons/plus.svg" alt="plus" width={24} height={24} />
-            <h2 className="text-14 font-semibold text-gray-600">Add Bank</h2>
+            <h2 className="text-14 font-semibold text-gray-600">
+              Add Bank</h2>
           </Link>
         </div>
+
+         {banks.Length > 0 && (
+          <div className="relative flex flex-1 flex-col items-center justify-center gap-5">
+            <div className="relative z-10">
+              BANK CARD 1
+            </div>
+            {banks[1] && (
+              <div className="absolute right-0 top-8 z-0">
+                BANK CARD 2
+              </div>
+            )}
+          </div>
+         )} 
       </section>
     </aside>
   )
