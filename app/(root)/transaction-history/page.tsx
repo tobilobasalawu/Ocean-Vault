32e224484aa48a5f6273dc1b93d201a2fea51e1e
John Name <oluwatobilobasalawu1234@gmail.com>
2024-11-03 22:57:01 +0000

    creating a transaction histiry route for the page

---
+++ app/(root)/transaction-history/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TransactionHistory = () => {
+  return (
+    <section>
+      <h1>Transaction History</h1>
+    </section>
+  )
+}
+
+export default TransactionHistory;
