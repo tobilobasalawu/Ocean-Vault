ac5bee2de27e8d59cbe1d7df324b99cadce04317
John Name <oluwatobilobasalawu1234@gmail.com>
2024-11-04 17:25:37 +0000

    created user.actions.ts to use server-side rendering for sign-in and sign-up functionality

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,17 @@
+'use server';
+
+export const signIn = async () => {
+  try {
+    //Mutation / Database / Make fetch
+  } catch (error) {
+    console.log('Error', error)
+  }
+}
+
+export const signUp = async () => {
+  try {
+    //Mutation / Database / Make fetch
+  } catch (error) {
+    console.log('Error', error)
+  }
+}
