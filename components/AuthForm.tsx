a0cf0a7fb6cfd8ad015910737512a5dd9f5d9a97
John Name <oluwatobilobasalawu1234@gmail.com>
2024-11-04 00:46:57 +0000

    Refactor AuthForm component to include form validation using zod and react-hook-form

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -5,13 +5,43 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
 
 const formSchema = z.object({
-  username: z.string().min(2).max(50),
+  username: z.string().min(2, {
+    message:"Username must be at least 2 characters.",
+  }),
 });
 
 const AuthForm = ({type}: {type: string}) => {
   const [user, setUser] = useState(null);
+
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof formSchema>>({
+      resolver: zodResolver(formSchema),
+      defaultValues: {
+        username: "",
+      },
+    })
+   
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof formSchema>) {
+      // Do something with the form values.
+      // ✅ This will be type-safe and validated.
+      console.log(values)
+    }
 
   return (
     <section className = "auth-form">
@@ -46,7 +76,27 @@ const AuthForm = ({type}: {type: string}) => {
         </div>
       ): (
         <div>
-          FORM
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                <FormField
+                  control={form.control}
+                  name="username"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Username</FormLabel>
+                      <FormControl>
+                        <Input placeholder="shadcn" {...field} />
+                      </FormControl>
+                      <FormDescription>
+                        This is your public display name.
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button type="submit">Submit</Button>
+              </form>
+            </Form>
         </div>
       )}
 
