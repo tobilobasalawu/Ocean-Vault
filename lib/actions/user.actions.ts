7304bf5d21f64386ffba4e0a87bb488bbeba8970
John Name <oluwatobilobasalawu1234@gmail.com>
2024-11-05 19:58:09 +0000

    Refactor user.actions.ts for server-side rendering and session management (Sign-up)

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,4 +1,8 @@
 'use server';
+import { createSessionClient, createAdminClient } from './appwrite';
+import { ID } from 'node-appwrite';
+import { cookies } from 'next/headers';
+import { parseStringify } from '../utils';
 
 export const signIn = async () => {
   try {
@@ -8,10 +12,45 @@ export const signIn = async () => {
   }
 }
 
-export const signUp = async (userData: SignUpData) => {
+export const signUp = async (userData: SignUpParams) => {
+  const { email, password, firstName, lastName } = userData;
+
   try {
     //Create user in Appwrite
+      const { account } = await createAdminClient();
+
+      const newUserAccount = await account.create(
+        ID.unique(), 
+        email, 
+        password, 
+        `${firstName} ${lastName}`
+      );
+      const session = await account.createEmailPasswordSession(
+        email, 
+        password
+      );
+    
+      cookies().set("appwrite-session", session.secret, {
+        path: "/",
+        httpOnly: true,
+        sameSite: "strict",
+        secure: true,
+      });
+
+      return parseStringify(newUserAccount);
+
   } catch (error) {
     console.log('Error', error)
+  }
+}
+
+// ... your initilization functions
+
+export async function getLoggedInUser() {
+  try {
+    const { account } = await createSessionClient();
+    return await account.get();
+  } catch (error) {
+    return null;
   }
 }
